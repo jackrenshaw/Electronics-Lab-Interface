3f3e716470d5c94fcd059a74e47bdf932209326b
Jack Renshaw <jack.renshaw97@gmail.com>
2021-10-19 12:29:59 +1100

    push up a few things

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 const express = require("express");
 const path = require('path');
 
+const { spawn } = require('child_process');
+const ls = spawn('ngspice', ['rc.cir']);
+
+ls.stdout.on('data', (data) => {
+  console.log(`stdout: ${data}`);
+});
+
+ls.stderr.on('data', (data) => {
+  console.error(`stderr: ${data}`);
+});
+
+ls.on('close', (code) => {
+  console.log(`child process exited with code ${code}`);
+});
+
 const PORT = process.env.PORT || 5000
 app = express();
 
